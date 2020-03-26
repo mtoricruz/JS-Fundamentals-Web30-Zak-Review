13e9da773eff37917318970274fd0a6724346279
Zachary Mayfield <zakmayfield@Zacharys-MacBook-Pro.local>
2020-03-26 19:05:40 -0400

    created base index.js

---
+++ index.js
@@ -0,0 +1,98 @@
+/*
+Create a for loop that loops thorugh forLoopNumbers and multiplies each number by 2
+
+If a number is equal to 6 then multiply by 100
+*/
+
+const forLoopNumbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+function doubleMostNums(){
+
+}
+
+
+
+
+/*
+map through a 'random' array and multiply the numbers by 2
+
+If the element is a string then return `Whats in the {string}`
+
+If the element is true or false return `It's a boolean`
+*/
+
+const random = [1, 2, 3, 'booooox!?', 5, true]
+
+function whatsInTheBox(){
+
+}
+
+
+
+
+/*
+Create a car constructor
+
+Initialize your car with some sweet specs like a nickname, an engine like: 5.0 liter, DOHC V8, and a paintColor like: matte black that come from an object called atters
+
+Give your car the ability to drive that returns `vroooom vrooom`
+
+Give your car the ability to speak that returns `Nickname: ${nickname}, Color: ${paint}`
+
+create a car named myCar that's an instance of car and assign the values needed
+
+Get your car to drive and speak
+*/
+
+function Car () {
+
+}
+
+
+
+
+/*
+Create a Human class constructor that takes a sinlge object and providing these keys: name, age, hairColor
+
+The constructor will initialize name, age, and hairColor
+
+Instances of Human must be able to .think() -> thinking should return a simple math equation like `2 plus 2 equals ${equation}`
+*/
+
+class Human {
+
+}
+
+
+
+
+/*
+Create an instance of Human called Adult that takes an object 
+
+the constructor will initialize a new key called jobTitle
+
+Adults should be able to .think() and .speakJob()
+
+.speakJob() should return `I work as a ${jobTitle}`
+*/
+
+class Adult extends Human {
+
+}
+
+
+
+
+/*
+create an instance of Human called Child that takes an object
+
+the constructor will initialize a new key called peesTheBed and will eventually equal a boolean
+
+Child should be able to answer if they pee the bed or not with .doYouPeeTheBed()
+
+.doYouPeeTheBed() should return `I still pee the bed` if peesTheBed is true and should return `I don't pee the bed anymore` if peesTheBed is false
+*/
+
+class Child extends Human {
+
+}
